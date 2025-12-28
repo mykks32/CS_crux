5f11641d6a44e5c5440c9750e4aae0c8eee80aca
mykks32 <srikri5462ydv@gmail.com>
2025-12-28 14:24:41 +0545

    feat: Car Status Notifier OBSERVER pattern

---
+++ Design_Pattern/ts_dp3/src/CarObserver.ts
@@ -0,0 +1,27 @@
+import {ICarObserver} from "./interfaces/ICarObserver";
+
+export class InsuranceSystem implements ICarObserver {
+    public update(status: string): void {
+        console.log("Insurance notified: " + status);
+    }
+}
+
+export class ServiceCenter implements ICarObserver {
+    public update(status: string): void {
+        console.log("Service Center notified: " + status);
+    }
+}
+
+export class CarStatusNotifier {
+    private readonly observers: Array<ICarObserver> = [];
+
+    public addObserver(observer: ICarObserver): void {
+        this.observers.push(observer);
+    }
+
+    public notifyAll(status: string): void {
+        this.observers.forEach(observer => {
+            observer.update(status);
+        })
+    }
+}
---
+++ Design_Pattern/ts_dp3/src/enums/car-types-enum.ts
@@ -0,0 +1,4 @@
+export enum CarTypesEnum {
+    SEDAN = 'SEDAN',
+    SUV = 'SUV',
+}
