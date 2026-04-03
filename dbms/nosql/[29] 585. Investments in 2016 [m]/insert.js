a428fc58b31e00aeabae6201eeb1e6bb308014a0
Shree Krishna Yadav <srikri5462ydv@gmail.com>
2026-04-03 22:14:32 +0545

    feat(dbms/MongoDB): [29] 585. Investments in 2016 [m]

---
+++ dbms/nosql/[29] 585. Investments in 2016 [m]/insert.js
@@ -0,0 +1,7 @@
+// Insert Into Insurance Collection
+    db.Insurance.insertMany([
+        {pid: 1, tiv_2015: 10, tiv_2016: 5, lat: 10, lon: 10},
+        {pid: 2, tiv_2015: 20, tiv_2016: 20, lat: 20, lon: 20},
+        {pid: 3, tiv_2015: 10, tiv_2016: 30, lat: 20, lon: 20},
+        {pid: 4, tiv_2015: 10, tiv_2016: 40, lat: 40, lon: 40}
+        ])
